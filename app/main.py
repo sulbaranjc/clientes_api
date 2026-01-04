3640c830297bd3412815ecca138b6ee558ec9269
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2026-01-04 16:49:59 +0100

    Desactiva allow_credentials en CORS para mayor seguridad

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # en producción se ajusta
-    allow_credentials=True,
+    allow_credentials=False,  # False para permitir * en origins
     allow_methods=["*"],
     allow_headers=["*"],
 )
